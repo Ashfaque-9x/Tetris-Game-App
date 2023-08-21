59ea67f0dc850cd4bfad4c2b4e253736a5ce67f5
Ashfaque <ashfaque.s510@gmail.com>
2023-08-22 02:39:31 +0530

    changed the colour

--- tetris.js
+++ tetris.js
@@ -238,7 +238,7 @@ function drawGameOverScreen(){
 function drawMainMenu(){
     context.fillStyle='#303040'
     context.fillRect(0,0, canvas.width, canvas.height)
-    context.fillStyle='green'
+    context.fillStyle='white'
     context.font = '32px Russo One';
     context.textAlign = "center";
     context.fillText("Lets Play..", canvas.width /2, canvas.height /2)
